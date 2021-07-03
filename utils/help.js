f54603de190c8019e1ab1dec72f24eb53f3eab94
Max Puig <45133421+MaxPuig@users.noreply.github.com>
2021-07-04 00:16:02 +0200

    Specified Admin command and relay is hidden

--- utils/help.js
+++ utils/help.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 function sendHelpCommands(msg, prefix) {
     if (msg.content.toLowerCase() == `${prefix}help`) {
         let mensaje = '**Comandos:**\n';
-        mensaje += '`' + prefix + 'prefijo <nuevo prefijo>` para cambiar el prefijo de los comandos. Ejemplo `.prefijo !`\n';
+        mensaje += '`' + prefix + 'prefijo <nuevo prefijo>` para cambiar el prefijo de los comandos. Ejemplo `.prefijo !` (Solo admin)\n';
         mensaje += '`' + prefix + 'rss` para establecer ese canal de texto para recibir ofertas de juegos. `' + prefix + 'rss borrar` para deshacerlo. (Solo admins del server)\n'
         mensaje += '`' + prefix + 'lista` para empezar una lista de nombres. Hay que pulsar el botón para unirse\n'
-        mensaje += '`' + prefix + 'relay` info sobre como mandar mensajes entre servidores. (Solo admin)\n'
+        // mensaje += '`' + prefix + 'relay` info sobre como mandar mensajes entre servidores. (Solo admin)\n'
         mensaje += '`' + prefix + 'xgame` (Juego) para dimensiones aleatorias o `' + prefix + 'xgame 4x5` para tamaño personalizado (mínimo: 3x3, máximo: 5x5)\n'
         mensaje += 'Link de invitación: <' + process.env.INVITELINK + '>';
         // - Prefijo
